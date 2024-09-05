a6334c7ff5f61631bf4d553b10a57f127535dd31
lucas-fochesatto <lucas.fochesatto@gmail.com>
2024-09-05 13:18:25 -0300

    rip off

--- database.js
+++ database.js
@@ -10,15 +10,11 @@ const db = new sqlite3.Database('./database.db', (err) => {
 });
 
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS battles (id INTEGER PRIMARY KEY, maker INTEGER, taker INTEGER, maker_pokemons TEXT, maker_battling_pokemons TEXT, taker_pokemons TEXT, taker_battling_pokemons TEXT, maker_move TEXT, taker_move TEXT, status TEXT, current_turn INTEGER, battle_log TEXT)')
-  db.run('CREATE TABLE IF NOT EXISTS pokemons (id INTEGER PRIMARY KEY, name TEXT, type TEXT, hp INTEGER, attack INTEGER, defense INTEGER, speed INTEGER, moves TEXT, status TEXT)')
-  db.run('CREATE TABLE IF NOT EXISTS hashes (hash TEXT PRIMARY KEY)')
-
-  const insert = db.prepare('INSERT OR REPLACE INTO pokemons (id, name, type, hp, attack, defense, speed, moves, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)')
-  pokemons.forEach(pokemon => {
-    insert.run(pokemon.id, pokemon.name, pokemon.type, pokemon.hp, pokemon.attack, pokemon.defense, pokemon.speed, pokemon.moves, pokemon.status)
-  })
-  insert.finalize()
+  db.run('CREATE TABLE IF NOT EXISTS battles (id INTEGER PRIMARY KEY, maker INTEGER, taker INTEGER, maker_pokemons TEXT, maker_battling_pokemons TEXT, taker_pokemons TEXT, taker_battling_pokemons TEXT, maker_move TEXT, taker_move TEXT, status TEXT, current_turn INTEGER, battle_log TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS hashes (hash TEXT PRIMARY KEY)');
+  db.run('CREATE TABLE IF NOT EXISTS players (playerid INTEGER PRIMARY KEY, wallet TEXT, inventory TEXT, battles TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS pokemons (id INTEGER PRIMARY KEY, name TEXT, type TEXT, hp INTEGER, attack INTEGER, defense INTEGER, speed INTEGER, atk1 TEXT, atk2 TEXT, atk3 TEXT, image TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS battle_logs (id INTEGER PRIMARY KEY, battle_id TEXT, log TEXT)');
 })
 
 export default db;
